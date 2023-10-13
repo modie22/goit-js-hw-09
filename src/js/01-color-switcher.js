608e0bb519bb71bb6c35c055d8201799d99324f3
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-10-13 12:37:37 +0300

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  const startbtnEl = document.querySelector("button[data-start]");
+  const stopbtnEl = document.querySelector("button[data-stop]");
+  const bodyEl = document.querySelector("body");
 
+  startbtnEl.addEventListener('click',startFunction);
+  stopbtnEl.addEventListener('click',stopFunction)
+
+  function startFunction(e){
+    e.target.disabled=true;
+    timer = setInterval(()=>{
+    bodyEl.style.backgroundColor=getRandomHexColor();
+    },1000)
+  }
+  function stopFunction(e){
+    startbtnEl.disabled=false;
+    clearTimeout(timer);
+  }
